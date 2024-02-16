0afc6075e2f18b41031abcd66494ad85023cd48e
alexsameri <samerialexx@gmail.com>
2024-02-16 23:40:15 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './scss/App.scss'
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { Header } from './components'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
